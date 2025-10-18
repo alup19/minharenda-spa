34608f97f8fb2bcb840485b6d284cf4ebd9ebe08
Alberto <alup.gov@gmail.com>
2025-10-17 23:07:23 -0300

    feat: add Layout, Context and Utils structures

---
+++ src/utils/UsuarioType.ts
@@ -0,0 +1,10 @@
+export type UsuarioType = {
+    id: string
+    nome: string
+    email: string
+    cpf: string
+    celular: string
+    admin: boolean
+    vipLevel: number
+    senha: string
+}
